26d6ecf0ce31f3b379000a0882097f60153dcca0
ali <aliyavuz2000@hotmail.com>
2022-07-20 10:05:32 -0400

    finished up to last user story

--- supers/serializers.py
+++ supers/serializers.py
@@ -4,4 +4,5 @@ from .models import Super
 class SuperSerializer(serializers.ModelSerializer):
   class Meta:
     model = Super
-    fields = ['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase','super_type']
+    fields = ['name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
+    depth = 1
--- supers/views.py
+++ supers/views.py
@@ -3,13 +3,17 @@ from rest_framework.response import Response
 from .serializers import SuperSerializer
 from .models import Super
 from rest_framework.decorators import api_view
-
+from rest_framework import status
 
 # Create your views here.
 @api_view(['GET','POST'])
 def super_methods(request):
   if request.method == 'GET':
+    hero_type= request.query_params.get('super_type')
+    print(hero_type)
     supers_all = Super.objects.all()
+    if hero_type:
+      supers_all = supers_all.filter(super_type__type=hero_type)
     serializer = SuperSerializer(supers_all, many = True)
     return Response(serializer.data)
 
@@ -17,9 +21,9 @@ def super_methods(request):
     serializer = SuperSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def super_by_id(request,pk):
   supers = get_object_or_404(Super, pk=pk)
   if request.method == 'GET':
@@ -30,3 +34,6 @@ def super_by_id(request,pk):
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(serializer.data)
+  elif request.method =='DELETE':
+    supers.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
