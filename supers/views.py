05479f438bb2c78a354ac7ac28a0745413d4a5a2
ali <aliyavuz2000@hotmail.com>
2022-07-20 10:06:17 -0400

    final changes

--- supers/views.py
+++ supers/views.py
@@ -5,6 +5,7 @@ from .models import Super
 from rest_framework.decorators import api_view
 from rest_framework import status
 
+
 # Create your views here.
 @api_view(['GET','POST'])
 def super_methods(request):
@@ -17,11 +18,13 @@ def super_methods(request):
     serializer = SuperSerializer(supers_all, many = True)
     return Response(serializer.data)
 
+
   elif request.method =='POST':
     serializer = SuperSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def super_by_id(request,pk):
@@ -29,11 +32,13 @@ def super_by_id(request,pk):
   if request.method == 'GET':
     serializer = SuperSerializer(supers)
     return Response(serializer.data)
+
   elif request.method =='PUT':
     serializer = SuperSerializer(supers, data=request.data, )
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(serializer.data)
+    
   elif request.method =='DELETE':
     supers.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
